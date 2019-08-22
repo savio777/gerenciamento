62e0ab6ba383703e1cdf683c91d3f26dd243d9dc
Savio <saviopf22@gmail.com>
2019-08-22 17:19:21 -0300

    bug da lista resolvida

--- local/src/pages/Caixa.js
+++ local/src/pages/Caixa.js
@@ -20,11 +20,22 @@ import add from '../../assets/icons8-mais-24.png'
 
 const Caixa = ({ navigation }) => {
 
-  const listTest = [
-    { desc: 'iae kk', val: 10.5, tipo: 'entrada' },
-    { desc: 'boa noite', val: 35.1, tipo: 'saida' },
-    { desc: 'bom dia kk', val: 12.1, tipo: 'entrada' }
-  ]
+  const [desc, setDesc] = useState('')
+  const [valor, setValor] = useState('')
+  const [quantidade, setQuantidade] = useState('')
+  const [tipo, setTipo] = useState('')
+
+  const [fluxo, setFluxo] = useState([])
+
+  const handleFluxo = () => {
+    setFluxo([...fluxo, { id: `${fluxo.length}`, desc: desc, val: valor, quantidade: quantidade }])
+  }
+
+  /*const listTest = [
+    { desc: 'iae kk', val: 10.5, quantidade: 4, tipo: 'entrada' },
+    { desc: 'boa noite', val: 35.1, quantidade: 2, tipo: 'saida' },
+    { desc: 'bom dia kk', val: 12.1, quantidade: 8, tipo: 'entrada' }
+  ]*/
 
   return (
     <>
@@ -34,25 +45,49 @@ const Caixa = ({ navigation }) => {
           <TextInput
             style={styles.input}
             placeholder='Descrição'
+            onChangeText={setDesc}
+            value={desc}
           />
           <TextInput
             style={styles.input}
             placeholder='Valor'
-            autoCompleteType='off'
             keyboardType='number-pad'
+            onChangeText={setValor}
+            value={valor}
+          //value={`${valor}`}
           />
-          <Picker>
+          <TextInput
+            style={styles.input}
+            placeholder='Quantidade'
+            keyboardType='number-pad'
+            onChangeText={setQuantidade}
+            value={quantidade}
+          //value={`${quantidade}`}
+          />
+          {/* FALTA IMPLEMENTAR O CAMPO TIPO */}
+          {/*<Picker
+            selectedValue={tipo}
+            onValueChange={setTipo}
+          >
             <Picker.Item label='Tipo' value='' />
             <Picker.Item label='Entrada' value='entrada' />
             <Picker.Item label='Saida' value='saida' />
-          </Picker>
-          <TouchableOpacity style={styles.buttonAdd}>
+          </Picker>*/}
+          <TouchableOpacity
+            style={styles.buttonAdd}
+            onPress={handleFluxo}
+          >
             <Image source={add} />
           </TouchableOpacity>
+
           <ScrollView>
-            <FlatList data={listTest} 
-              renderItem={({item})=><Text>{item.desc}, {item.val}, {item.tipo}</Text>} />
+            <FlatList
+              data={fluxo}
+              renderItem={({ item }) => <Text>{item.desc}, {item.val}, {item.quantidade}, {item.tipo}</Text>}
+              keyExtractor={(item, index) => item.id}
+            />
           </ScrollView>
+
         </View>
       </SafeAreaView>
     </>
@@ -69,7 +104,13 @@ const styles = StyleSheet.create({
 
   },
   input: {
-    marginTop: 10
+    marginTop: 20,
+    paddingHorizontal: 30,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#2C3A47',
+    textAlign: 'center',
+    backgroundColor: '#fff'
   },
   inputSelect: {
     marginTop: 10
